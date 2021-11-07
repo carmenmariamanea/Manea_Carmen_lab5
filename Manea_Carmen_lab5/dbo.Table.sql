2980d4bc7b05280d2a11771603c663077af8bf82
Dell <Dell@DESKTOP-AI3B91T>
2021-11-07 14:04:31 +0200

    Add project files.

---
+++ Manea_Carmen_lab5/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[CarId] INT NOT NULL PRIMARY KEY, 
+    [Make] NVARCHAR(50) NULL, 
+    [COLOR] NVARCHAR(50) NULL
+)
---
+++ Manea_Carmen_lab5/dbo.Table_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[CustId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL
+)
